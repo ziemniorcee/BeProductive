723463195117de21a8cb735673f11059c3cff8c2
Igor <ziemniorcee@gmail.com>
2023-04-16 06:46:06 +0200

    First screen v2

--- Data.py
+++ Data.py
@@ -3,20 +3,22 @@ import requests
 
 
 class Date:
+    """Class for returning Date elements"""
     def __init__(self):
         self.today = datetime.datetime.now()
-        print(self.today)
-
         self.hour1 = self.today.hour
+
         self.get_date()
 
     def get_date(self):
+        """assing to self.day date in format (YYYY month DD)"""
         months = ["January", "February", "March", "April", "May", "June", "July",
                   "August", "September", "October", "November", "December"]
         self.day = f"{self.today.year} {months[self.today.month - 1]} {self.today.day}"
 
 
 class Weather:
+    """Class for returning weather contitions"""
     def __init__(self, h):
         self.OMW_Endpoint = "https://api.openweathermap.org/data/2.8/onecall"
         self.api_key = "27fddb32ad49da904b460c57943f6d92"
@@ -34,6 +36,7 @@ class Weather:
         self.image1()
 
     def get_weather(self):
+        """getting weather. In comment Api version"""
         # response = requests.get(self.OMW_Endpoint, params=self.weather_params)
         # self.info = response.json()["current"]
         self.info = {'dt': 1681561339, 'sunrise': 1681530929, 'sunset': 1681580932, 'temp': 9.88, 'feels_like': 9.47,
@@ -42,20 +45,15 @@ class Weather:
                      'weather': [{'id': 501, 'main': 'Rain', 'description': 'moderate rain', 'icon': '10d'}],
                      'rain': {'1h': 1.06}}
 
-        print(self.info)
-        temp = self.info["temp"]
-        temp_f = self.info["feels_like"]
+        temp, temp_f = self.info["temp"], self.info["feels_like"]
         self.temperature = [f"{round(temp)}{self.degree_sign}", f"{round(temp_f)}{self.degree_sign}"]
 
     def image1(self):
+        """choosing proper image  dependently on the weather"""
         self.type = int(self.info["weather"][0]["id"])
-        rise_time = self.info["sunrise"]
-        set_time = self.info["sunset"]
 
-        print(rise_time)
-        rise_time = int(datetime.datetime.fromtimestamp(rise_time).strftime("%I"))
-        set_time = int(datetime.datetime.fromtimestamp(set_time).strftime("%I"))+12
-        print(rise_time, set_time, self.hour)
+        rise_time = int(datetime.datetime.fromtimestamp(self.info["sunrise"]).strftime("%I"))
+        set_time = int(datetime.datetime.fromtimestamp(self.info["sunset"]).strftime("%I")) + 13
 
         if not rise_time < self.hour < set_time:
             self.image = "images/wi-night-clear.png"
--- main.py
+++ main.py
@@ -6,16 +6,15 @@ BG_COLOR = "#242424"
 FONT_COLOR = "#fcf7ff"
 SECOND_COLOR = "#2C0049"
 
-customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
-
 FONT = ("Arial", 30)
 
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
+        self.welcome_window()
 
+    def welcome_window(self):
         today = Date()
         date = today.day
         hour = today.hour1
@@ -30,26 +29,34 @@ class App(customtkinter.CTk):
         self.grid_rowconfigure(1, weight=1)
         self.grid_rowconfigure(2, weight=3)
 
-        label_date = customtkinter.CTkLabel(self, text=f" {date} ", font=FONT, text_color=FONT_COLOR)
-        label_date.grid(row=0, column=1)
+        label_head = customtkinter.CTkLabel(self, text=f" {date} ", font=FONT, text_color=FONT_COLOR, )
+        label_head.grid(row=0, column=1)
 
-        canvas = customtkinter.CTkCanvas(self, width=400, height=300, bg=BG_COLOR, highlightthickness=0)
-        canvas.grid(row=1, column=1)
+        self.canvas = customtkinter.CTkCanvas(self, width=400, height=300, bg=BG_COLOR, highlightthickness=0)
+        self.canvas.grid(row=1, column=1)
 
         img = Image.open(weather_data.image)
         img = img.resize((300, 300))
         self.img = ImageTk.PhotoImage(img)
 
-        canvas.create_image(200, 150, image=self.img)
+        self.canvas.create_image(200, 150, image=self.img)
 
         # label_weather = customtkinter.CTkLabel(self, text=text, font=FONT_1)
-        canvas.create_text(200, 150, text=f"{weather_data.temperature[0]}", font=FONT, fill=FONT_COLOR)
-        canvas.create_text(200, 180, text=f"Feels like: {weather_data.temperature[1]}", font=("Arial", 15), fill=FONT_COLOR)
+        self.canvas.create_text(200, 150, text=f"{weather_data.temperature[0]}", font=FONT, fill=FONT_COLOR)
+        self.canvas.create_text(200, 180, text=f"Feels like: {weather_data.temperature[1]}", font=("Arial", 15),
+                                fill=FONT_COLOR)
 
-        main_button = customtkinter.CTkButton(self, text="Start your day", fg_color="transparent", font=FONT,
-                                              border_width=2, border_color=SECOND_COLOR,
-                                              text_color=("gray10", "#DCE4EE"))
-        main_button.grid(row=2, column=0, columnspan=2)
+        self.main_button = customtkinter.CTkButton(self, text="Start your day", fg_color="transparent", font=FONT,
+                                                   border_width=2, border_color=SECOND_COLOR,
+                                                   text_color=("gray10", "#DCE4EE"), command=self.second_window)
+        self.main_button.grid(row=2, column=0, columnspan=2)
+
+    def second_window(self):
+        self.canvas.delete("all")
+        self.main_button.destroy()
+        self.geometry(f"{500}x{700}")
+
+
 
 
 if __name__ == "__main__":
