e0fc13a3a7390f83ab62b1c53cdae81b08495619
Igor <ziemniorcee@gmail.com>
2023-04-17 19:03:53 +0200

    Second Screen 1st version

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.welcome_window()
-        self.second_window()
+        # configure window
+        self.title("Better Tomorrow")
+        self.geometry(f"{500}x{700}")
 
     def welcome_window(self):
         today = Date()
@@ -22,9 +24,7 @@ class App(customtkinter.CTk):
 
         weather_data = Weather(hour)
 
-        # configure window
-        self.title("Better Tomorrow")
-        self.geometry(f"{500}x{700}")
+
 
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(1, weight=1)
@@ -42,7 +42,6 @@ class App(customtkinter.CTk):
 
         self.canvas.create_image(200, 150, image=self.img)
 
-        # label_weather = customtkinter.CTkLabel(self, text=text, font=FONT_1)
         self.canvas.create_text(200, 150, text=f"{weather_data.temperature[0]}", font=FONT, fill=FONT_COLOR)
         self.canvas.create_text(200, 180, text=f"Feels like: {weather_data.temperature[1]}", font=("Arial", 15),
                                 fill=FONT_COLOR)
@@ -61,37 +60,68 @@ class App(customtkinter.CTk):
         self.grid_rowconfigure(1, weight=0)
         self.grid_rowconfigure(2, weight=0)
 
-        self.canvas2 = customtkinter.CTkCanvas(self, width=400, height=100, bg=BG_COLOR, highlightthickness=0)
-        self.canvas2.grid(row=0, column=1)
-        self.canvas2.create_text(200, 25, text="GOALS FOR TODAY", font=FONT, fill=FONT_COLOR)
+        # 1st block
+        self.canvas_head = customtkinter.CTkCanvas(self, width=500, height=100, bg=BG_COLOR, highlightthickness=0)
+        self.canvas_head.grid(row=0, column=1)
+        self.canvas_head.create_text(250, 25, text="GOALS FOR TODAY", font=FONT, fill=FONT_COLOR)
         img = Image.open("images/line.png")
+        img = img.resize((450, 100))
         self.img = ImageTk.PhotoImage(img)
-        self.canvas2.create_image(200, 50, image=self.img)
+        self.canvas_head.create_image(250, 50, image=self.img)
 
-        self.canvas3 = customtkinter.CTkCanvas(self, width=500, height=600, bg=BG_COLOR, highlightthickness=0)
-        self.canvas3.grid(row=1, column=1)
-        self.goals = []
-        for i in range(5):
-            goal = self.canvas3.create_text(10, 50 + i * 60, text=f"{str(i + 1)}. ", font=("Arial", 20), fill=FONT_COLOR, anchor="w")
-            self.goals.append(goal)
+        # 2nd block
+        self.canvas_list = customtkinter.CTkCanvas(self, width=500, height=450, bg=BG_COLOR, highlightthickness=0)
+        self.canvas_list.grid(row=1, column=1)
+        self.create_goals()
 
         self.entry_widget = customtkinter.CTkEntry(self, font=("Arial", 20))
-        self.canvas3.create_window(220, 400, window=self.entry_widget, width=370, height=40)
+        self.canvas_list.create_window(220, 400, window=self.entry_widget, width=370, height=40)
         self.button_widget = customtkinter.CTkButton(self, text="+", font=("Arial", 40), fg_color=SECOND_COLOR,
                                                      command=self.add_goal)
-        self.canvas3.create_window(450, 400, window=self.button_widget, height=40, width=40)
+        self.canvas_list.create_window(450, 400, window=self.button_widget, height=40, width=40)
+        self.entry_widget.bind('<Return>', self.add_goal)
 
-    def add_goal(self):
+        reg = self.register(self.callback)
+        self.entry_widget.configure(validate="key", validatecommand=(reg, '%P'))
+
+    def create_goals(self):
+        self.goals = []
+        for i in range(5):
+            goal = self.canvas_list.create_text(10, 50 + i * 60, text=f"{str(i + 1)}. ", font=("Arial", 20),
+                                                fill=FONT_COLOR, anchor="w")
+            self.goals.append(goal)
+
+    def add_goal(self, *_):
         value = self.entry_widget.get()
         for goal in self.goals:
-            x = self.canvas3.itemcget(goal, 'text')
+            x = self.canvas_list.itemcget(goal, 'text')
 
             if len(x) < 4:
-                self.canvas3.itemconfigure(goal)
-                self.canvas3.itemconfigure(goal, text=f"{x}{value}")
+                self.canvas_list.itemconfigure(goal)
+                self.canvas_list.itemconfigure(goal, text=f"{x}{value}")
                 self.entry_widget.delete(0, 100)
                 break
 
+        if len(self.canvas_list.itemcget(self.goals[4], 'text')) > 3:
+            print("xd")
+            self.accept_goals()
+            self.entry_widget.unbind("<Return>")
+
+    def callback(self, input1):
+        if len(input1) < 30:
+            return True
+        else:
+            return False
+
+    def accept_goals(self):
+        self.entry_widget.configure(state="disabled")
+        self.button_widget.configure(state="disabled")
+        self.accept_canvas = customtkinter.CTkCanvas(self, width=500, height=150, bg=BG_COLOR, highlightthickness=0)
+        self.accept_canvas.grid(row=2, column=1)
+
+        self.accept_canvas.create_text(250, 25, text="These are my goals", font=FONT, fill=FONT_COLOR)
+        self.yes_button = customtkinter.CTkButton(self, text="+", font=("Arial", 40), fg_color=SECOND_COLOR)
+        self.accept_canvas.create_window(150, 100, window=self.yes_button, height=40, width=40)
 
 if __name__ == "__main__":
     app = App()
